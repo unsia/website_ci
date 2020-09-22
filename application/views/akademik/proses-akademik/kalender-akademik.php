397486f7fed437ee7519dea4372bd3d21af03141
unsia <mypc804@gmail.com>
2020-09-22 16:06:39 +0700

    update akademik

--- application/views/akademik/proses-akademik/kalender-akademik.php
+++ application/views/akademik/proses-akademik/kalender-akademik.php
@@ -1,162 +1,140 @@
 <!-- Page Content Box ==== -->
 <div class="content-block">
-    <!-- Our Story ==== -->
-    <div class="section-area section-sp1 our-story">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 m-b30">
-                    <div class="feature-container">
-                        <div class="icon-content mb-2">
-                            <p style="text-align: justify">
-                                Bagi mahasiswa Universitas Siber Asia info kegiatan akademik dalam satu
-                                tahun akademik, semester ganjil dan genap silahkan melihat info berikut ini:
-                            </p>
-                            <p style="text-align: justify">
-                                Kegiatan akademik Semester Ganjil 2020/2021:
-                                <h6>1. Pengenalan Sistem Akademik (PSA) Universitas Siber Asia</h6>
-                                <div class="ml-4">
-                                    Kegiatan PSA diberikan untuk setiap mahasiswa baru dalam rangka
-                                    memberikan pemahaman kuliah online di UNSIA meliputi: </br>
-                                    a. Pengenalan sistem dan tata kelola UNSIA online </br>
-                                    b. Pengenalan LMS (Learning Management System) </br>
-                                    c. Pengenalan Bimbingan Online </br>
-                                    d. Pengenalan Metode Pembelajaran Online </br>
-                                    e. Pengenalan pelatihan Online </br>
-                                    f. Pengenalan sistem pendukung utama layanan akademik dan kemahasiswaan
-                                    </br>
-                                </div>
-                            </p>
-                            <div class="row">
-                                <div class="col-md-2"></div>
-                                <div class="col-md-8">
-                                    <table class="table">
-                                        <thead style="background-color: #A5C1E1">
-                                            <tr>
-                                                <th scope="col">No</th>
-                                                <th scope="col">Kegiatan</th>
-                                                <th scope="col">Waktu</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr class="text-left">
-                                                <td>1</td>
-                                                <td>Pengenalan Sistem Akademik (PSA) UNSIA</td>
-                                                <td>Minggu ke-3 Agustus 2020</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div class="col-md-2"></div>
-                            </div>
-                            <div class="ml-4"><a href="#">Klik link berikut untuk PSA UNSIA Online</a></div>
-                            <p style="text-align: justify">
-                                <h6>2. Jadwal Akademik Semester Ganjil 2020/2021</h6>
-                            </p>
-                            <div class="row">
-                                <div class="col-md-2"></div>
-                                <div class="col-md-8">
-                                    <table class="table">
-                                        <thead style="background-color: #A5C1E1">
-                                            <tr>
-                                                <th scope="col">No</th>
-                                                <th scope="col">Kegiatan</th>
-                                                <th scope="col">Waktu</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr class="text-left">
-                                                <td>1</td>
-                                                <td>Bimbingan Akademik</td>
-                                                <td>Minggu ke 4 Agustus 2020</td>
-                                            </tr>
-                                            <tr class="text-left">
-                                                <td>2</td>
-                                                <td>Perkuliahan Online Tahap I (7 minggu)</td>
-                                                <td>Minggu ke 1 September 2020 – Minggu ke-3 Oktober 2020
-                                                </td>
-                                            </tr>
-                                            <tr class="text-left">
-                                                <td>3</td>
-                                                <td>Minggu Tenang I</td>
-                                                <td>Minggu ke-4 Oktober 2020</td>
-                                            </tr>
-                                            <tr class="text-left">
-                                                <td>4</td>
-                                                <td>UTS (UJIAN TENGAH SEMESTER)</td>
-                                                <td>Minggu ke-1 dan ke-3 November 2020</td>
-                                            </tr>
-                                            <tr class="text-left">
-                                                <td>5</td>
-                                                <td>Perkuliahan Online Tahap II (7 minggu)</td>
-                                                <td>Minggu ke-1 Desember 2020 – Minggu ke-3 Januari 2021
-                                                </td>
-                                            </tr>
-                                            <tr class="text-left">
-                                                <td>6</td>
-                                                <td>Minggu Tenang II</td>
-                                                <td>Minggu ke-4 Januari 2021</td>
-                                            </tr>
-                                            <tr class="text-left">
-                                                <td>7</td>
-                                                <td>UAS (UJIAN AKHIR SEMESTER)</td>
-                                                <td>Minggu ke-1 dan ke-2 Februari 2021</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div class="col-md-2"></div>
-                            </div>
-                            <p style="text-align: justify">
-                                <h6>3. Kegiatan Pelatihan</h6>
-                                <div class="ml-4">
-                                    Setiap semester UNSIA menyelenggarakan pelatihan Online, pelatian
-                                    tersebut bersertifikat dan setiap mahasiswa diwajibkan mengikuti:
-                                </div>
-                            </p>
-                            <div class="row">
-                                <div class="col-md-2"></div>
-                                <div class="col-md-8">
-                                    <table class="table">
-                                        <thead style="background-color: #A5C1E1">
-                                            <tr>
-                                                <th scope="col">No</th>
-                                                <th scope="col">Kegiatan</th>
-                                                <th scope="col">Waktu</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr class="text-left">
-                                                <td>1</td>
-                                                <td>Pengenalan Pemrograman Online</td>
-                                                <td>Minggu ke-4 Februari 2021</td>
-                                            </tr>
-                                            <tr class="text-left">
-                                                <td>2</td>
-                                                <td>Pengenalan SPSS Online</td>
-                                                <td>Minggu ke-4 Februari 2021</td>
-                                            </tr>
-                                            <tr class="text-left">
-                                                <td>3</td>
-                                                <td>Pengenalan Data Sains</td>
-                                                <td>Minggu ke-4 Februari 2021</td>
-                                            </tr>
-                                            <tr class="text-left">
-                                                <td>4</td>
-                                                <td>Pengenalan IoT</td>
-                                                <td>Minggu ke-4 Februari 2021</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div class="col-md-2"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Our Story END ==== -->
+	<!-- Our Story ==== -->
+	<div class="section-area section-sp1 our-story">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 m-b30">
+					<div class="feature-container">
+						<div class="icon-content mb-2">
+							<h4 class="text-center">SEMESTER GANJIL 2020/2021</h4>
+							<div class="row">
+								<div class="col">
+									<div class="table-responsive">
+										<table class="table">
+											<thead style="background-color: #A5C1E1">
+												<tr>
+													<th scope="col" class="text-center">No</th>
+													<th scope="col" class="text-center">Kegiatan</th>
+													<th scope="col" class="text-center">Tanggal</th>
+													<th scope="col" class="text-center">Jumlah Hari/Minggu</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr class="text-left">
+													<td>1</td>
+													<td>Pengisian KRS (Online)</td>
+													<td>07 September - 10 Oktober 2020</td>
+													<td>5 Minggu</td>
+												</tr>
+												<tr class="text-left">
+													<td>2</td>
+													<td>Pengenalan Sistem Akademik UNSIA (Online)</td>
+													<td>22 Oktober – 23 Oktober 2020</td>
+													<td>2 Hari</td>
+												</tr>
+												<tr class="text-left">
+													<td>3</td>
+													<td>Perkuliahan Tahap I (Online)</td>
+													<td>26 Oktober -12 Desember 2020</td>
+													<td>7 Minggu</td>
+												</tr>
+												<tr class="text-left">
+													<td>4</td>
+													<td>Laporan PD DIKTI</td>
+													<td>12 Oktober – 14 Oktober 2020</td>
+													<td>3 Hari</td>
+												</tr>
+												<tr class="text-left">
+													<td>5</td>
+													<td>Kuliah Pengganti (Online)</td>
+													<td>14 Desember – 16 Desember 2020</td>
+													<td>3 Hari</td>
+												</tr>
+												<tr class="text-left">
+													<td>6</td>
+													<td>Persiapan UTS</td>
+													<td>17 Desember – 19 Desember 2020</td>
+													<td>3 Hari</td>
+												</tr>
+												<tr class="text-left">
+													<td>7</td>
+													<td>Ujian Tengah Semester (Online)</td>
+													<td>21 Desember – 24 Desember 2020</td>
+													<td>4 Hari</td>
+												</tr>
+												<tr class="text-left">
+													<td>8</td>
+													<td>Masa pengisian Nilai Tugas 1 dan UTS (Online)</td>
+													<td>21 Desember -29 Desember 2020</td>
+													<td>2 Minggu</td>
+												</tr>
+												<tr class="text-left">
+													<td>9</td>
+													<td>Perkuliahan Tahap II (Online)</td>
+													<td>4 Januari – 20 Februari 2021</td>
+													<td>7 Minggu</td>
+												</tr>
+												<tr class="text-left">
+													<td>10</td>
+													<td>Kuliah Pengganti (Online)</td>
+													<td>22 Februarui - 24 Februari 2021</td>
+													<td>3 Hari</td>
+												</tr>
+												<tr class="text-left">
+													<td>11</td>
+													<td>Persiapan UAS </td>
+													<td>25 Februari - 27 Februari 2021</td>
+													<td>3 Hari</td>
+												</tr>
+												<tr class="text-left">
+													<td>12</td>
+													<td>Ujian Akhir Semester (Online)</td>
+													<td>01 Februari - 06 Maret 2021</td>
+													<td>1 Minggu</td>
+												</tr>
+												<tr class="text-left">
+													<td>13</td>
+													<td>Masa Pengisian Nilai Tugas 2 dan UAS (Online)</td>
+													<td>01 Februari - 13 Maret 2021</td>
+													<td>2 Minggu</td>
+												</tr>
+												<tr class="text-left">
+													<td>14</td>
+													<td>Libur Antar Semester <br> Program Pelatihan/Workshop online</td>
+													<td>08 Maret - 20 Mareti 2021 <br> (Tentative)</td>
+													<td>2 Minggu</td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+							<p class="text-right">Jakarta, 7 Oktober 2020</p>
+							<hr>
+							<div class="row">
+								<div class="col-12 col-md-4">
+									<table>
+										<tr>
+											<td><small><b>KRS</b></small></td>
+											<td><small><b>=</b></small></td>
+											<td><small><b>Kartu Rencana Studi</b></small></td>
+										</tr>
+										<tr>
+											<td><small><b>PD DIKTI</b></small></td>
+											<td><small><b>=</b></small></td>
+											<td><small><b>Pangkalan Data Perguruan Tinggi</b></small></td>
+										</tr>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Our Story END ==== -->
 </div>
 <!-- Page Content Box END ==== -->
 </div>
