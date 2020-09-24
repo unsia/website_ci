2ab9a3aeeb01822cbac03e465c5d7f3ca08e3dcd
unsia <mypc804@gmail.com>
2020-09-24 12:11:17 +0700

    update pixel fb

--- application/views/marketing.php
+++ application/views/marketing.php
@@ -3,12 +3,23 @@
 
 <head>
 	<!-- Google Tag Manager -->
+	<script>
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-57QZG9P');
+	</script>
 
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-57QZG9P');</script>
 	<script>
 		"trackEvent": {
 			"on": "click",
@@ -19,9 +30,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 			}
 		},
 	</script>
-
 	<!-- End Google Tag Manager -->
-
 	<!-- META ============================================= -->
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -76,11 +85,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 </head>
 
 <body id="bg">
-	
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57QZG9P"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57QZG9P" height="0" width="0"
+			style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	<div class="page-wraper">
 		<div id="loading-icon-bx"></div>
 		<!-- Content -->
--- application/views/partial/prodi_akuntansi_header.php
+++ application/views/partial/prodi_akuntansi_header.php
@@ -4,82 +4,104 @@
 <!-- Mirrored from educhamp.themetrades.com/demo/admin/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 22 Feb 2019 13:08:15 GMT -->
 
 <head>
+<!-- Google Tag Manager -->
+<script>
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-57QZG9P');
+	</script>
+	<!-- End Google Tag Manager -->
+	<!-- META ============================================= -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
 
-    <!-- META ============================================= -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords" content="" />
-    <meta name="author" content="" />
-    <meta name="robots" content="" />
+	<!-- DESCRIPTION -->
+	<meta name="description" content="Universitas Siber Asia" />
 
-    <!-- DESCRIPTION -->
-    <meta name="description" content="Universitas Siber Asia" />
+	<!-- OG -->
+	<meta property="og:title" content="Universitas Siber Asia" />
+	<meta property="og:description" content="Universitas Siber Asia" />
+	<meta property="og:image" content="" />
+	<meta name="format-detection" content="telephone=no">
 
-    <!-- OG -->
-    <meta property="og:title" content="Universitas Siber Asia" />
-    <meta property="og:description" content="Universitas Siber Asia" />
-    <meta property="og:image" content="" />
-    <meta name="format-detection" content="telephone=no">
+	<!-- FAVICONS ICON ============================================= -->
+	<link rel="icon" href="../error-404.html" type="image/x-icon" />
+	<link rel="shortcut icon" type="image/x-icon" href="<?= base_url(); ?>assets/prodi/images/favicon.png" />
 
-    <!-- FAVICONS ICON ============================================= -->
-    <link rel="icon" href="../error-404.html" type="image/x-icon" />
-    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url(); ?>assets/prodi/images/favicon.png" />
+	<!-- PAGE TITLE HERE ============================================= -->
+	<title><?= $prodi; ?> - <?= $title; ?></title>
 
-    <!-- PAGE TITLE HERE ============================================= -->
-    <title><?= $prodi; ?> - <?= $title; ?></title>
+	<!-- MOBILE SPECIFIC ============================================= -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- MOBILE SPECIFIC ============================================= -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- All PLUGINS CSS ============================================= -->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/assets.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/vendors/calendar/fullcalendar.css">
 
-    <!-- All PLUGINS CSS ============================================= -->
-    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/assets.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/vendors/calendar/fullcalendar.css">
+	<!-- TYPOGRAPHY ============================================= -->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/typography.css">
 
-    <!-- TYPOGRAPHY ============================================= -->
-    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/typography.css">
+	<!-- SHORTCODES ============================================= -->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/shortcodes/shortcodes.css">
 
-    <!-- SHORTCODES ============================================= -->
-    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/shortcodes/shortcodes.css">
+	<!-- STYLESHEETS ============================================= -->
+	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/style.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/dashboard.css">
+	<link class="skin" rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/color/color-1.css">
 
-    <!-- STYLESHEETS ============================================= -->
-    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/style.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/dashboard.css">
-    <link class="skin" rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/prodi/css/color/color-1.css">
-
-    <link rel="stylesheet" href="https://cdn.plyr.io/3.6.2/plyr.css" />
+	<link rel="stylesheet" href="https://cdn.plyr.io/3.6.2/plyr.css" />
 
 </head>
 
 <body class="ttr-opened-sidebar ttr-pinned-sidebar">
-
-    <!-- header start -->
-    <header class="ttr-header">
-        <div class="ttr-header-wrapper">
-            <!--sidebar menu toggler start -->
-            <div class="ttr-toggle-sidebar ttr-material-button">
-                <i class="ti-close ttr-open-icon"></i>
-                <i class="ti-menu ttr-close-icon"></i>
-            </div>
-            <!--sidebar menu toggler end -->
-            <!--logo start -->
-            <div class="ttr-logo-box">
-                <div>
-                    <a href="<?= base_url(); ?>" class="ttr-logo">
-                        <img class="ttr-logo-mobile" alt="" src="<?= base_url(); ?>assets/images/logo-mobile.png" width="30" height="30">
-                        <img class="ttr-logo-desktop" alt="" src="<?= base_url(); ?>assets/images/logo-white.png" width="160" height="27">
-                    </a>
-                </div>
-            </div>
-            <!--logo end -->
-            <div class="ttr-header-menu">
-                <!-- header left menu start -->
-                <ul class="ttr-header-navigation">
-                    <li>
-                        <a href="" class="ttr-material-button ttr-submenu-toggle">Program Studi - <?= $prodi; ?></a>
-                    </li>
-                </ul>
-                <!-- header left menu end -->
-            </div>
-        </div>
-    </header>
-    <!-- header end -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57QZG9P" height="0" width="0"
+			style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<!-- header start -->
+	<header class="ttr-header">
+		<div class="ttr-header-wrapper">
+			<!--sidebar menu toggler start -->
+			<div class="ttr-toggle-sidebar ttr-material-button">
+				<i class="ti-close ttr-open-icon"></i>
+				<i class="ti-menu ttr-close-icon"></i>
+			</div>
+			<!--sidebar menu toggler end -->
+			<!--logo start -->
+			<div class="ttr-logo-box">
+				<div>
+					<a href="<?= base_url(); ?>" class="ttr-logo">
+						<img class="ttr-logo-mobile" alt="" src="<?= base_url(); ?>assets/images/logo-mobile.png"
+							width="30" height="30">
+						<img class="ttr-logo-desktop" alt="" src="<?= base_url(); ?>assets/images/logo-white.png"
+							width="160" height="27">
+					</a>
+				</div>
+			</div>
+			<!--logo end -->
+			<div class="ttr-header-menu">
+				<!-- header left menu start -->
+				<ul class="ttr-header-navigation">
+					<li>
+						<a href="" class="ttr-material-button ttr-submenu-toggle">Program Studi - <?= $prodi; ?></a>
+					</li>
+				</ul>
+				<!-- header left menu end -->
+			</div>
+		</div>
+	</header>
+	<!-- header end -->
