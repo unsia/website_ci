1a2546902e01f7bf13dbf8d15344a5d84ba3b2e5
unsia <mypc804@gmail.com>
2021-04-19 10:21:25 +0700

    e

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/website_ci/';
+$config['base_url'] = 'http://unsia.ac.id/';
 
 /*
 |--------------------------------------------------------------------------
